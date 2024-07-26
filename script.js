dc8857978f2c0419aa822ea9c5a2268d19f833c6
Camryn Brown <108500312+camrynbrown@users.noreply.github.com>
2024-07-26 01:30:25 -0400

    Added play round function.
    Additionally changed the logic of determining
    the choice.

--- script.js
+++ script.js
@@ -6,20 +6,78 @@ console.log("Hello World!!")
 
 function getComputerChoice() {
     // randomly return one of the choices
-    let number = Math.floor(Math.random() * 3)
+    let number = Math.floor((Math.random() * 3) + 1)
+    console.log("Computer Chose: ")
     switch (number) {
-        case 0:
-            return console.log("Rock")
         case 1:
-            return console.log("Scissors")
+            console.log("Rock")
+            return number
         case 2:
-            return console.log("Paper")
+            console.log("Paper")
+            return number
+        case 3:
+            console.log("Scissors")
+            return number
     }
 }
 
 function getHumanChoice() {
     let choice = prompt("Rock, Paper, or Scissors?")
-    alert("You chose " + choice)
-
-    return choice
+    // alert("You chose " + choice)
+    choice = choice.toUpperCase()
+    
+    switch (choice) {
+        case "ROCK":
+            choice = 1
+            return choice
+        case "PAPER":
+            choice = 2
+            return choice
+        case "SCISSORS":
+            choice = 3
+            return choice
+        default:
+            return alert("That is not a valid choice.")
+    }
 }
+
+function playRound(humanChoice, computerChoice) {
+    // computerChoice = computerChoice.toUpperCase()
+    console.log("Human choice: " + humanChoice + " Computer choice " + computerChoice)
+    
+    let message1 = "You lose!"
+    let message2 = "You win!"
+    if (humanChoice == 1 && computerChoice == 2) {
+        console.log(message1 + " Paper beats rock.")
+    }
+
+    else if (humanChoice == 1 && computerChoice == 3) {
+        console.log (message2 + " Rock beats scissors!!")
+    }
+
+    else if (humanChoice == 2 && computerChoice == 1) {
+        console.log (message2 + " Paper beats rock!!")
+    }
+
+    else if (humanChoice == 2 && computerChoice == 3) {
+        console.log(message1 + " Scissors beats paper.")
+    }
+
+    else if (humanChoice == 3 && computerChoice == 1) {
+        console.log(message1 + " Rock beats scissors.")
+    }
+
+    else if (humanChoice == 3 && computerChoice == 2) {
+        console.log(message2 + " Scissors beats paper.")
+    }
+
+    else if (humanChoice == computerChoice) {
+        console.log(" It is a tie.")
+    }
+
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection);
